9964b2e1cd08bc26398839824eca7ec454624655
parimal-art <parimalmaity852@gmail.com>
2025-04-08 20:54:20 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,25 +1,18 @@
 const express = require("express");
-
 const app = express();
 
-app.use("/user",(req, res)=>{
-    res.send("use handle all the request and remember order matters!!")
-})
-
-app.get("/user",(req, res)=>{
-    res.send({firstName:"Parimal", lastName:"Maity"})
-})
-app.post("/user",(req, res)=>{
-    res.send("Data is successfilly posted")
-})
-app.delete("/user",(req, res)=>{
-    res.send("Data is successfilly Deleted")
-})
-app.use("/test",(req, res)=>{
-    res.send("This is an testing API call")
-})
-
-app.listen(3000, ()=>{
-    console.log("the port is listening in port 3000");
-    
+// ✅ This RegExp matches both /ac and /abc
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+    res.send("This is an param call using multiple param");
 });
+
+app.get("/", (req, res) => {
+    res.send("Home route");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+
