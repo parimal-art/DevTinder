d52e3b2ee40927a4bd47bb619561446d8e961bcd
parimal-art <parimalmaity852@gmail.com>
2025-04-09 10:28:24 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const app = express();
 
-// ✅ This RegExp matches both /ac and /abc
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    res.send("This is an param call using multiple param");
-});
+app.use("/user",[
+    (req, res, next) => {
+        console.log("this is the 1st handle request");
+        next();
+        // res.send("Home route 1 ")
+    },
+    (req, res, next) => {
+        console.log("this is the 2nd handle request");
+        next();
+        // res.send("Home route 2 ")
+    }],
+    (req, res, next) => {
+        console.log("this is the 3Rd handle request");
+        next();
+        // res.send("Home route 3 ")
+    },
+    (req, res, next) => {
+        console.log("this is the 4TH handle request");
+        next();
+        res.send("request handled")
+    }
 
-app.get("/", (req, res) => {
-    res.send("Home route");
-});
+)
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
